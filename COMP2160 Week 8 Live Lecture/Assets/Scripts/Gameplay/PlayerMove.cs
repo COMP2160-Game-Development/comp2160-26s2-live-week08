a14ffa89238af2f5af8556faaad0472dd56028a6
Malcolm Ryan <malcolm.ryan@mq.edu.au>
2026-09-15 12:28:16 +1000

    fix{gameplay]: Fix player movement speed

--- COMP2160 Week 8 Live Lecture/Assets/Scripts/Gameplay/PlayerMove.cs
+++ COMP2160 Week 8 Live Lecture/Assets/Scripts/Gameplay/PlayerMove.cs
@@ -13,7 +13,7 @@ public class PlayerMove : MonoBehaviour
 
 #region Parameters
     [Tooltip("m/s")]
-    [SerializeField] private float speed = 1f;
+    [SerializeField] private float speed = 5f;
 #endregion 
 
 #region Components
