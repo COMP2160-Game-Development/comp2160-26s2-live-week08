638d8913eca97521bc36cdcf5b45ea4ee3649399
KaysonWhitehouse <109711622+KaysonWhitehouse@users.noreply.github.com>
2026-09-15 12:15:55 +1000

    Feat[UI]: Implement UI Manager

---
+++ COMP2160 Week 8 Live Lecture/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,76 @@
+/**
+ * 
+ * Author: Malcolm Ryan
+ * Version: 1.0
+ * For Unity Version: 6000.0.53f1
+ */
+
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+
+#region Parameters
+
+
+
+#endregion 
+
+#region Connected Objects
+#endregion
+
+#region Components
+#endregion
+
+#region State
+#endregion
+
+#region Properties
+static private UIManager instance;
+static public UIManager Instance
+{
+    get
+    {
+        if (instance == null)
+        {
+            Debug.LogError("No UI Manager");
+            return instance;
+        }
+    
+    }
+}
+
+#endregion
+
+#region Events
+#endregion
+
+#region Init & Destroy
+    void Awake()
+    {
+    }
+#endregion 
+
+#region Update
+    void Update()
+    {
+    }
+#endregion
+
+#region FixedUpdate
+    void FixedUpdate()
+    {        
+    }
+#endregion
+
+#region Gizmos
+    void OnDrawGizmos()
+    {
+        if (!Application.isPlaying)
+        {
+            // Don't run in the editor
+            return;
+        }
+    }
+#endregion
+}
